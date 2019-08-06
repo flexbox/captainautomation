538b4d34fde9952820ad1408549db8eac9567507
David Leuliette <dleuliette@gmail.com>
2019-08-06 08:32:41 +0200

    Add mdx deck

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,29 @@
 module.exports = {
   plugins: [
     {
+      resolve: "gatsby-theme-mdx-deck",
+      options: {
+        mdx: true,
+        // source directory
+        contentPath: "decks",
+        // base path for routes generate by this theme
+        basePath: "workshop",
+      },
+    },
+    {
       resolve: `gatsby-theme-blog`,
       options: {},
     },
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
+    title: `Captain Automation`,
+    author: `David Leuliette`,
     description: `My site description...`,
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        url: `https://twitter.com/flexbox_`,
       },
       {
         name: `github`,
