f968856d96d5e2ca03d4475ab5b51ac42a68d80b
David Leuliette <dleuliette@gmail.com>
2019-08-07 20:58:21 +0200

    Add airtable link

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,12 @@ module.exports = {
     {
       resolve: "gatsby-theme-mdx-deck",
       options: {
+        // enable or disable gatsby-plugin-mdx
         mdx: true,
         // source directory
         contentPath: "decks",
         // base path for routes generate by this theme
-        basePath: "workshop",
+        basePath: "",
       },
     },
     {
