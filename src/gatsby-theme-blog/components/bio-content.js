61e771d1732795ba78cff37066d304ebac0cc0fa
David Leuliette <dleuliette@gmail.com>
2019-09-01 23:45:11 +0200

    💥 add linkedin content

--- src/gatsby-theme-blog/components/bio-content.js
+++
@@ -1,8 +0,0 @@
-import React, { Fragment } from "react"
-
-export default () => (
-  <Fragment>
-    Manual Work is a Bug. <br />
-    Always be automating, change your mindset and achieve more in less time.
-  </Fragment>
-)
